1a622363b2eb69df7af808abdd28ed52f6c4d72f
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2022-03-25 16:56:35 +0800

    bot: connected to websocket

--- internal/bot/init.go
+++ internal/bot/init.go
@@ -1,6 +1,44 @@
 package bot
 
+import (
+	"log"
+	"net/http"
+	"net/url"
+
+	"github.com/gorilla/websocket"
+)
 
 func Init() {
+	socketInit()
+}
+
+func socketInit() {
+	u := url.URL{
+		Scheme: "ws",
+		Host:   "localhost:8080",
+		Path:   "/message",
+	}
+
+	header := http.Header{
+		"verifyKey": {"1234567890"},
+		"qq":        {"2473537565"},
+	}
+
+	log.Printf("connecting to %s", u.String())
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), header)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+
+	for {
+		_, message, err := c.ReadMessage()
+		if err != nil {
+			log.Fatal("read:", err)
+		}
+		log.Printf("recv: %s", message)
+	}
+}
+
+func databaseInit() {
 
 }
---
+++ test/init_test.go
@@ -0,0 +1,10 @@
+package test_test
+
+import (
+	"pixie/internal/bot"
+	"testing"
+)
+
+func TestInit(t *testing.T) {
+	bot.Init()
+}
