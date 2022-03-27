a65dc78454e4660462f599c0b5cdec1b8e09ecaa
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2022-03-27 16:54:42 +0800

    handler: init handler

---
+++ internal/bot/handler.go
@@ -0,0 +1,13 @@
+package bot
+
+import (
+	"pixie/internal/pkg/json"
+)
+
+func friendMessageHandler(messageChain json.MessageChain) (json.WsReqData, error) {
+	return json.WsReqData{}, nil
+}
+
+func groupMessageHandler(messageChain json.MessageChain) (json.WsReqData, error) {
+	return json.WsReqData{}, nil
+}
--- internal/bot/server.go
+++ internal/bot/server.go
@@ -32,30 +32,99 @@ func Serve() {
 }
 
 func serveMessage(data []byte) Message {
-	messageChain, err := json.Decode(data, true)
+	messageChainItf, err := json.Decode(data, true)
+
 	if err != nil {
 		log.Printf("%s", err)
+		return Message{
+			Empty: true,
+		}
 	}
-	debug.DPrintf("decode: %v", messageChain)
+	debug.DPrintf("decode: %v", messageChainItf)
 
+	messageChain, ok := messageChainItf.(json.MessageChain)
+	if !ok {
+		panic("message chain type error!")
+	}
 
-	
+	var messageHandler func(json.MessageChain) (json.WsReqData, error)
+
+	switch messageChain.Type {
+	case "FriendMessage":
+		messageHandler = friendMessageHandler
+	case "GroupMessage":
+		messageHandler = groupMessageHandler
+	default:
+		log.Printf("unknown message chain type")
+		return Message{
+			Empty: true,
+		}
+	}
+
+	wsReq, err := messageHandler(messageChain)
+	if err != nil {
+		return Message{
+			Empty: true,
+		}
+	}
+
+	wsReqBytes, err := json.Encode(wsReq)
+	if err != nil {
+		return Message{
+			Empty: true,
+		}
+	}
 
 	return Message{
-		Empty: true,
+		Empty: false,
+		Done:  make(chan struct{}),
+		Data:  wsReqBytes,
 	}
 }
 
 func serveEvent(data []byte) Event {
-	event, err := json.Decode(data, false)
+	eventItf, err := json.Decode(data, false)
 	if err != nil {
 		log.Printf("%s", err)
+		return Event{
+			Empty: true,
+		}
 	}
 
-	debug.DPrintf("decode: %v", event)
+	debug.DPrintf("decode: %v", eventItf)
 
+	event, ok := eventItf.(json.Event)
+	if !ok {
+		panic("message chain type error!")
+	}
+
+	var eventHandler func(json.Event) (json.WsReqData, error)
+
+	switch event.Type {
+	default:
+		log.Printf("unknown event type")
+		return Event{
+			Empty: true,
+		}
+	}
+
+	wsReq, err := eventHandler(event)
+	if err != nil {
+		return Event{
+			Empty: true,
+		}
+	}
+
+	wsReqBytes, err := json.Encode(wsReq)
+	if err != nil {
+		return Event{
+			Empty: true,
+		}
+	}
 
 	return Event{
-		Empty: true,
+		Empty: false,
+		Done:  make(chan struct{}),
+		Data:  wsReqBytes,
 	}
 }
