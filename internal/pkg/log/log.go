b542df6f2850c314c3c01a342eef5682dee03a09
El-even-11 <zhao_ziqian@sjtu.edu.cn>
2022-04-06 22:40:23 +0800

    log: fix file path

--- internal/pkg/log/log.go
+++ internal/pkg/log/log.go
@@ -11,7 +11,7 @@ import (
 func Log(format string, a ...interface{}) {
 	ymd := time.Now().Format("2006-01-02")
 	ymdhms := time.Now().Format("2006-01-02 15:04:05")
-	file, err := os.OpenFile("./logs/"+ymd, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, err := os.OpenFile("../logs/"+ymd, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	defer file.Close()
 	if err != nil {
 		log.Printf("open file failed")
